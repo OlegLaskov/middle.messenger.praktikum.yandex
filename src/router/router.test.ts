4f3954c0a86c994d985bb6cb00afc3a153196d99
Oleg <olaskov@gmail.com>
2022-08-19 23:30:30 +0300

    added Component tests

--- src/router/router.test.ts
+++ src/router/router.test.ts
@@ -2,8 +2,9 @@ import { expect } from "chai";
 import Router from ".";
 import Component from "../utils/component";
 
-describe('Проверяем переходы у Роута', () => {
-	it('Переход на новую страницу должен менять состояние сущности history', () => {
+describe('Router testing', () => {
+	it('Тестим руты', () => {
+		// window.history.pushState({page: 'login'}, 'Login', 'http://localhost');
 		// window.history.pushState({page: 'login'}, 'Login', '/');
 		// window.history.pushState({page: 'sign-up'}, 'Sign up', '/sign-up');
 		// window.history.pushState({page: 'error404'}, 'Error 404', '/error404');
@@ -13,9 +14,27 @@ describe('Проверяем переходы у Роута', () => {
 				super('div', {attr:{id: 'home'}});
 			}
 		}
+		class Login extends Component {
+			constructor(){
+				super('p', {attr:{id: 'login'}});
+			}
+		}
+		class Signup extends Component {
+			constructor(){
+				super('span', {attr:{id: 'signup'}});
+			}
+		}
 		router.use('/', Home);
-		router.start();
+		router.use('/login', Login);
+		router.use('/signup', Signup);
+		// router.start();
+		// router.go('/login');
+		// router.go('/signup');
+		// router.back();
+		// router.back();
 	
-		expect(window.history.length).to.eq(3);
+		expect(router.getRoute('/')?.match('/')).to.eq(true);
+		expect(router.getRoute('/login')?.match('/login')).to.eq(true);
+		expect(router.getRoute('/signup')?.match('/signup')).to.eq(true);
 	});
-  }); 
+}); 
